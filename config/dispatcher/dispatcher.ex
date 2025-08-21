11d53e512b5e9f14d2a87acef701bf4877d4eb02
Vandekeybus Niels <Niels.Vandekeybus@district09.gent>
2025-08-21 17:44:08 +0200

    make some changes to work on qa.stad.gent

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -20,17 +20,17 @@ defmodule Dispatcher do
   define_layers [ :static, :sparql, :api_services, :frontend_fallback, :resources, :not_found ]
 
  # frontend
-  get "/assets/*path", %{ layer: :static } do
-    forward conn, path, "http://frontend/assets/"
+  get "/data/assets/*path", %{ layer: :static } do
+    forward conn, path, "http://frontend/data/assets/"
   end
 
-  get "/index.html", %{ layer: :static } do
-    forward conn, [], "http://frontend/index.html"
+  get "/data/index.html", %{ layer: :static } do
+    forward conn, [], "http://frontend/data/index.html"
   end
 
-  get "/sparql", %{ layer: :static, accept: %{ html: true} } do
-    forward conn, [], "http://frontend/index.html"
-  end
+  # get "/sparql", %{ layer: :static, accept: %{ html: true} } do
+  #   forward conn, [], "http://frontend/index.html"
+  # end
 
   # API SERVICES
   match "/resource-labels/*path", %{ layer: :api_services, accept: %{ json: true } } do
@@ -41,14 +41,27 @@ defmodule Dispatcher do
     forward conn, path, "http://uri-info/"
   end
 
-  match "/sparql/*path", %{ layer: :sparql, accept: %{ sparql: true} } do
-    forward conn, path, "http://sparqlproxy/sparql/"
+  match "/conductor/*path", %{ layer: :sparql } do
+    forward conn, path, "http://virtuoso:8890/conductor/"
   end
 
+  match "/sparql/*path", %{ layer: :sparql } do
+    forward conn, path, "http://virtuoso:8890/sparql/"
+  end
+
+  get "/data/views/*path", %{ layer: :static, accept: %{ html: true } } do
+    forward conn, path, "http://frontend/data/"
+  end
+
+  match "/data/*path", %{ layer: :sparql } do
+    forward conn, path, "http://virtuoso:8890/data/"
+  end
+
+  
  # fallback routes
   get "/*path", %{ layer: :frontend_fallback, accept: %{ html: true } } do
-    # We forward path for fastboot
-    forward conn, path, "http://frontend/"
+    # We forward path virtuoso
+    forward conn, path, "http://virtuoso:8890/"
   end
 
   match "/*_", %{ layer: :not_found } do
